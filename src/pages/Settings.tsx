2ce4df412c4e60c3ab8e86f8eba4f9098e66d6e4
Linh Le <lemylinh237@gmail.com>
2024-08-15 15:10:10 +0300

    Added Setting and Authentication page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,69 @@
+import { useMutation, useQuery } from '@apollo/client';
+import React, { useContext } from 'react';
+
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import ImageUpload from '../components/Forms/FormElements/ImageUpload';
+import { AuthContext } from "../context/authContext";
+import { DELETE_USER, SINGLE_USER } from "../queries/queries";
+import UpdateSocialMedia from './SettingsElements/UpdateSocialMedia';
+import UpdateUser from './SettingsElements/UpdateUser';
+import UpdatePassword from './SettingsElements/UpdatePassword';
+
+const Settings = () => {
+  const auth = useContext(AuthContext)
+  const { loading, error, data } = useQuery(SINGLE_USER, {
+    variables: { userId: auth && auth.userId }
+  })
+
+  const userDetail = data?.getUserDetail
+  const [ deleteUser ] = useMutation(DELETE_USER)
+
+  const deleteAccountHandler = async() => {
+    try {
+      const response = await deleteUser({
+        variables: {
+          userId: auth.userId
+      }})
+      if (response && response.data && response.data.deleteUser) {
+        auth.logout()
+      }
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <>
+      <div className="mx-auto max-w-270">
+        <Breadcrumb pageName="Settings" />
+
+        <div className="grid grid-cols-5 gap-8">
+          <div className="col-span-5 xl:col-span-3">
+            <UpdateUser 
+              data={data}
+              userDetail={userDetail}
+            />
+            <UpdatePassword />
+          </div>
+          <div className="col-span-5 xl:col-span-2">
+            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+              <div className="border-b border-stroke py-4 px-7 dark:border-strokedark">
+                <h3 className="font-medium text-black dark:text-white">
+                  Your Photo
+                </h3>
+              </div>
+              <ImageUpload />
+            </div>
+
+            <UpdateSocialMedia 
+              data={data}
+              userDetail={userDetail}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Settings;
